91530a22fe539c0ebf1c380424f11e74afdc4cec
Phips Peter <pspeter333@gmail.com>
2020-03-31 12:00:18 -0700

    Add Webpack
    Add basic webpack configuration.
    
    Closes #2

---
+++ webpack.config.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const configure = (): webpack.Configuration => {
+    return {
+        entry: path.join(__dirname, "src", "index.ts"),
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: "ts-loader",
+                            options: {
+                                compilerOptions: {
+                                    module: "esnext"
+                                }
+                            }
+                        }
+                    ]
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Monster Stadium",
+                scriptLoading: "defer"
+            })
+        ]
+    };
+};
+
+export default configure;
