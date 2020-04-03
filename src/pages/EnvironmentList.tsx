c3f769630f84763af079ba4d78ba4945b9182bb1
Phips Peter <pspeter3@users.noreply.github.com>
2020-04-02 22:10:30 -0700

    Remove wouter-preact (#36)
    Reset the codebase for a single route app.

--- src/pages/EnvironmentList.tsx
+++
@@ -1,5 +0,0 @@
-import { h, FunctionComponent } from "preact";
-
-export const EnvironmentList: FunctionComponent = () => (
-    <h1>EnvironmentList</h1>
-);
--- src/pages/about.tsx
+++
@@ -1,3 +0,0 @@
-import { h, FunctionComponent } from "preact";
-
-export const About: FunctionComponent = () => <h1>About</h1>;
