fef24cb6d814a1908b6bbf0fea3c418b0c3a97be
Phips Peter <pspeter333@gmail.com>
2020-04-04 10:18:28 -0700

    Run format
    Run format on the code. Should have done this last time.

--- src/bestiary.ts
+++ src/bestiary.ts
@@ -18,21 +18,155 @@ export interface Source {
     readonly name: string;
 }
 
-export type Size = "Tiny" | "Small" | "Medium" | "Large" | "Huge" | "Gargantuan";
+export type Size =
+    | "Tiny"
+    | "Small"
+    | "Medium"
+    | "Large"
+    | "Huge"
+    | "Gargantuan";
 
-export const Sizes = new Set<Size>(["Tiny", "Small", "Medium", "Large", "Huge", "Gargantuan"]);
+export const Sizes = new Set<Size>([
+    "Tiny",
+    "Small",
+    "Medium",
+    "Large",
+    "Huge",
+    "Gargantuan",
+]);
 
-export type Alignment = "Any" | "Any Chaotic" | "Any Evil" | "Chaotic Evil" | "Chaotic Good" | "Chaotic Neutral" | "Lawful Evil" | "Lawful Good" | "Lawful Neutral" | "Neutral" | "Neutral Evil" | "Neutral Good" | "Non-good" | "Non-lawful" | "Unaligned";
+export type Alignment =
+    | "Any"
+    | "Any Chaotic"
+    | "Any Evil"
+    | "Chaotic Evil"
+    | "Chaotic Good"
+    | "Chaotic Neutral"
+    | "Lawful Evil"
+    | "Lawful Good"
+    | "Lawful Neutral"
+    | "Neutral"
+    | "Neutral Evil"
+    | "Neutral Good"
+    | "Non-good"
+    | "Non-lawful"
+    | "Unaligned";
 
-export const Alignments = new Set<Alignment>(["Any", "Any Chaotic", "Any Evil", "Chaotic Evil", "Chaotic Good", "Chaotic Neutral", "Lawful Evil", "Lawful Good", "Lawful Neutral", "Neutral", "Neutral Evil", "Neutral Good", "Non-good", "Non-lawful", "Unaligned"]);
+export const Alignments = new Set<Alignment>([
+    "Any",
+    "Any Chaotic",
+    "Any Evil",
+    "Chaotic Evil",
+    "Chaotic Good",
+    "Chaotic Neutral",
+    "Lawful Evil",
+    "Lawful Good",
+    "Lawful Neutral",
+    "Neutral",
+    "Neutral Evil",
+    "Neutral Good",
+    "Non-good",
+    "Non-lawful",
+    "Unaligned",
+]);
 
-export type Environment = "Aquatic" | "Arctic" | "Cave" | "Coast" | "Desert" | "Dungeon" | "Forest" | "Grassland" | "Mountain" | "Planar" | "Ruins" | "Swamp" | "Underground" | "Urban";
+export type Environment =
+    | "Aquatic"
+    | "Arctic"
+    | "Cave"
+    | "Coast"
+    | "Desert"
+    | "Dungeon"
+    | "Forest"
+    | "Grassland"
+    | "Mountain"
+    | "Planar"
+    | "Ruins"
+    | "Swamp"
+    | "Underground"
+    | "Urban";
 
-export const Environments = new Set<Environment>(["Aquatic", "Arctic", "Cave", "Coast", "Desert", "Dungeon", "Forest", "Grassland", "Mountain", "Planar", "Ruins", "Swamp", "Underground", "Urban"]);
+export const Environments = new Set<Environment>([
+    "Aquatic",
+    "Arctic",
+    "Cave",
+    "Coast",
+    "Desert",
+    "Dungeon",
+    "Forest",
+    "Grassland",
+    "Mountain",
+    "Planar",
+    "Ruins",
+    "Swamp",
+    "Underground",
+    "Urban",
+]);
 
-export type ChallengeRating = 0 | 0.125 | 0.25 | 0.5 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 26 | 30;
+export type ChallengeRating =
+    | 0
+    | 0.125
+    | 0.25
+    | 0.5
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 7
+    | 8
+    | 9
+    | 10
+    | 11
+    | 12
+    | 13
+    | 14
+    | 15
+    | 16
+    | 17
+    | 18
+    | 19
+    | 20
+    | 21
+    | 22
+    | 23
+    | 24
+    | 26
+    | 30;
 
-export const ChallengeRatings = new Set<ChallengeRating>([0, 0.125, 0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 26, 30]);
+export const ChallengeRatings = new Set<ChallengeRating>([
+    0,
+    0.125,
+    0.25,
+    0.5,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    26,
+    30,
+]);
 
 export interface Monster {
     readonly id: string;
